f8d58caa3d9ca0452da6f6180b0e482713b06089
João Afonso Santos <afonsooliveirasantos2003@gmail.com>
2025-10-05 15:26:46 +0100

    Implement text extraction from pdf

---
+++ src/pdfExtractor.py
@@ -0,0 +1,10 @@
+import pdfplumber
+
+def extract_pdf_text(filename):
+    with pdfplumber.open(filename) as pdf:
+        all_text = ""
+        for page in pdf.pages:
+            all_text += page.extract_text()
+    return all_text
+
+allText = extract_pdf_text("Nomeacoes_02a09Out.pdf")
